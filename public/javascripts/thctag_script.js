5b3ed261cfe5888160fd523ac36ff3edd783e6f2
Michele Barbera <barbz@perrin.local>
2009-07-02 12:22:51 +0200

    added demo img and philospace JS and CSS

---
+++ public/javascripts/thctag_script.js
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL /philospace_temp/jstest/js/thctag_script.js was not found on this server.</p>
+<hr>
+<address>Apache/2.2.3 (Debian) DAV/2 SVN/1.4.2 mod_jk/1.2.18 PHP/5.2.0-8+etch13 mod_ssl/2.2.3 OpenSSL/0.9.8c mod_perl/2.0.2 Perl/v5.8.8 Server at dbin.org Port 80</address>
+</body></html>
